f1c6f0e12cf9af5e3b439b99d0a67b386f9e90da
Teoh TeikToe <teohteiktoe@gmail.com>
2026-03-19 07:41:52 +0000

    1

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
 
+@app.route("/main",methods=["GET","POST"])
+def main():
+    return(render_template("main.html"))
+
+@app.route("/dbs",methods=["GET","POST"])
+def dbs():
+    return(render_template("dbs.html"))
+
 @app.route("/prediction",methods=["GET","POST"])
 def prediction():
     q = float(request.form.get("q"))
@@ -13,6 +21,10 @@ def prediction():
     pred = model.predict([[q]])
     r = pred[0][0]
     return(render_template("prediction.html", r=r))
+
+@app.route("/transferMoney",methods=["GET","POST"])
+def transferMoney():
+    return(render_template("transferMoney.html"))
 
 if __name__ == "__main__":
     app.run()
